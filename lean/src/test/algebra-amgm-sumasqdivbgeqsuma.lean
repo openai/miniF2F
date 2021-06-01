c226ea356cfa7049ae4838ab84766148c9cf1206
Stanislas Polu <spolu@openai.com>
2021-06-01 13:24:21 +0200

    [lean] more renaming

--- lean/src/test/algebra-amgm-sumasqdivbgeqsuma.lean
+++ lean/src/test/algebra-amgm-sumasqdivbgeqsuma.lean
@@ -5,7 +5,10 @@ Authors: Kunhao Zheng
 -/
 import data.real.basic
 
-example (a b c d : ℝ) (h₀ : 0 < a ∧ 0 < b ∧ 0 < c ∧ 0 < d) : a ^ 2 / b + b ^ 2 / c + c ^ 2 / d + d ^ 2 / a ≥ a + b + c + d :=
+theorem algebra_amgm_sumasqdivbgeqsuma
+  (a b c d : ℝ)
+  (h₀ : 0 < a ∧ 0 < b ∧ 0 < c ∧ 0 < d) :
+  a ^ 2 / b + b ^ 2 / c + c ^ 2 / d + d ^ 2 / a ≥ a + b + c + d :=
 begin
   sorry
 end
--- lean/src/test/algebra-sqineq-unitcircatbpamblt1.lean
+++ lean/src/test/algebra-sqineq-unitcircatbpamblt1.lean
@@ -5,7 +5,10 @@ Authors: Kunhao Zheng
 -/
 import data.real.basic
 
-example (a b: ℝ) (h₀ : a^2 + b^2 = 1 ) : a * b + (a - b) ≤ 1 :=
+theorem algebra_sqineq_unitcircatbpamblt1
+  (a b: ℝ)
+  (h₀ : a^2 + b^2 = 1 ) :
+  a * b + (a - b) ≤ 1 :=
 begin
   sorry
 end
