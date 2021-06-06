5087b5ed2051a0cb2fb204fddb321728573c9188
Stanislas Polu <spolu@openai.com>
2021-06-06 19:38:35 +0200

    [lean] GPT-f proof

--- lean/src/valid/algebra_sqineq_2unitcircatblt1.lean
+++ lean/src/valid/algebra_sqineq_2unitcircatblt1.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2021 OpenAI. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kunhao Zheng
+Authors: Kunhao Zheng, OpenAI GPT-f
 -/
 import data.real.basic
 
@@ -10,5 +10,9 @@ theorem algebra_sqineq_2unitcircatblt1
   (h₀ : a^2 + b^2 = 2) :
   a * b ≤ 1 :=
 begin
-  sorry
+  have hu := sq_nonneg a,
+  have hv := sq_nonneg b,
+  have H₁ := add_nonneg hu hv,
+  have H₂ : 0 ≤ (a - b) ^ 2 := by nlinarith,
+  nlinarith,
 end
