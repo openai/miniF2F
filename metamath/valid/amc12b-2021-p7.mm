2d0a56ddc068f7807d1a5a35b488e26e4e3204a6
Kunhao ZHENG <39208702+DyeKuu@users.noreply.github.com>
2021-06-07 18:37:21 +0200

    fix readme (#14)

--- metamath/valid/amc12b-2021-p7.mm
+++
@@ -1,9 +0,0 @@
-$(
-  @{
-    amc12b-2021-p7.0 @e |- ( ph -> X e. RR ) $@
-    amc12b-2021-p7.1 @e |- ( ph -> Y e. RR ) $@
-    @( (Contributed by Kunhao Zheng, 3-Jun-2021.) @)
-    amc12b-2021-p7 @p |- ( ph -> 1 <_ ( ( ( ( X x. Y ) - 1 ) ^ 2 ) + ( ( X + Y ) ^ 2 ) ) ) @=
-      ? @.
-  @}
-$)
