57f4f7d6dfb5ef2a0033767f62fcf6ec64f74ae7
Stanislas Polu <spolu@openai.com>
2021-06-09 18:26:23 +0200

    [lean] add algebra_sqineq_unitcircatbpabsamblt1

---
+++ lean/src/test/algebra_sqineq_unitcircatbpabsamblt1.lean
@@ -0,0 +1,15 @@
+/-
+Copyright (c) 2021 OpenAI. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Stanislas Polu
+-/
+import data.real.basic
+import analysis.normed_space.basic
+
+theorem algebra_sqineq_unitcircatbpabsamblt1
+  (a b: ℝ)
+  (h₀ : a^2 + b^2 = 1) :
+  a * b + ∥a - b∥ ≤ 1 :=
+begin
+  sorry
+end
