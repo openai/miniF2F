bd478677054f2eda01d3151af43d84aff715a52f
DyeKuu <dyekuu@qq.com>
2021-06-14 16:12:17 +0200

    copyright

--- lean/src/valid/mathd_numbertheory_412.lean
+++ lean/src/valid/mathd_numbertheory_412.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2021 OpenAI. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kunhao Zheng
+-/
 import data.real.basic
 
 theorem mathd_numbertheory_412
