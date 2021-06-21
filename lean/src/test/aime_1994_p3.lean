1cd3922e8dbbadb33cac5d46bbd1a599fb3ef247
scikud <scikud00@gmail.com>
2021-06-21 16:22:55 -0500

    some practice from 2 weeks ago

---
+++ lean/src/test/aime_1994_p3.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2021 OpenAI. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kudzo Ahegbebu
+-/
+
+import data.real.basic
+import data.int.modeq
+ 
+theorem aime_1994_p3
+  (x : ℤ  )
+  (f : ℤ  → ℤ )
+  (h0: f(x) + f(x-1) = x^2)
+  (h1: f(19) = 94):
+  f(94) % 1000 = 561 :=
+  begin
+    sorry,
+  end
