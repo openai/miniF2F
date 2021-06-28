cd615e56e4d8cac5a21ef3fcfa66197c3fac20d1
scikud <scikud00@gmail.com>
2021-06-28 16:38:25 -0700

    fixing grammatical issues in metamath

--- metamath/test/aime-1983-p1.mm
+++ metamath/test/aime-1983-p1.mm
@@ -1,17 +1,17 @@
 $(
   @{
-    aime-1983-p1.0 @e |- ( ph -> X e. NN) $@
-    aime-1983-p1.1 @e |- ( ph -> Y e. NN) $@
-    aime-1983-p1.2 @e |- ( ph -> Z e. NN) $@
+    aime-1983-p1.0 @e |- ( ph -> X e. NN ) $@
+    aime-1983-p1.1 @e |- ( ph -> Y e. NN ) $@
+    aime-1983-p1.2 @e |- ( ph -> Z e. NN ) $@
     aime-1983-p1.3 @e |- ( ph -> 1 < X ) $@
-    aime-1983-p1.4 @e |- ( ph -> 1 < Y) $@
-    aime-1983-p1.5 @e |- ( ph -> 1 < Z) $@
-    aime-1983-p1.6 @e |- ( ph -> W e. NN0) $@
-    aime-1983-p1.7 @e |- ( ph -> ( ( X log W ) = 24) ) $@
-    aime-1983-p1.8 @e |- ( ph -> ( ( Y log W) = 40) ) $@
-    aime-1983-p1.9 @e |- ( ph -> ( ( X x. (Y x. Z ) log W ) = 12 ) ) $@
+    aime-1983-p1.4 @e |- ( ph -> 1 < Y ) $@
+    aime-1983-p1.5 @e |- ( ph -> 1 < Z ) $@
+    aime-1983-p1.6 @e |- ( ph -> W e. NN0 ) $@
+    aime-1983-p1.7 @e |- ( ph -> ( X logb W ) = ; 2 4 ) $@
+    aime-1983-p1.8 @e |- ( ph -> ( Y logb W ) = ; 4 0 ) $@
+    aime-1983-p1.9 @e |- ( ph -> ( ( X x. ( Y x. Z ) logb W ) ) = ; 1 2 ) $@
     $( (Contributed by Kudzo  Ahegbebu, 28-Jun-2021.) $)
-    aime-1983-p1 @p |- (ph -> Z log W = ; 6 0) @=
+    aime-1983-p1 @p |- ( ph -> Z log W = ; 6 0) @=
      ? @.
   @}
 $)
