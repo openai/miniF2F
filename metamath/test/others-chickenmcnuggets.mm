e5aede11b50a924ac885cd31d2eb52ed70e6076e
DyeKuu <dyekuu@qq.com>
2021-08-09 12:41:20 +0200

    readme & clean

--- metamath/test/others-chickenmcnuggets.mm
+++
@@ -1,8 +0,0 @@
-$(
-  @{
-    others-chickenmcnuggets.0 @e |- ( ph -> N e. NN0 ) $@
-    @( (Contributed by Kunhao Zheng, 4-May-2021.) @)
-    others-chickenmcnuggets @p |- ( ph -> E. i e. NN0 E. j e. NN0 ( N + ; ; 1 5 2 ) = ( ( 9 x. i ) + ( ; 2 0 x. j ) ) ) @=
-      ? @.
-  @}
-$)
