3387df36a4ca5a85229c842ad4ea0da0727b99f1
DyeKuu <dyekuu@qq.com>
2021-08-10 11:09:36 +0200

    fix

--- metamath/test/mathd-algebra-598.mm
+++ metamath/test/mathd-algebra-598.mm
@@ -7,7 +7,7 @@ $(
     mathd-algebra-598.4 @e |- ( ph -> ( 4 ^c A ) = 5 ) $@
     mathd-algebra-598.5 @e |- ( ph -> ( 5 ^c B ) = 6 ) $@
     mathd-algebra-598.6 @e |- ( ph -> ( 6 ^c C ) = 7 ) $@
-    mathd-algebra-598.7 @e |- ( ph -> ( 7 ^c C ) = 8 ) $@
+    mathd-algebra-598.7 @e |- ( ph -> ( 7 ^c D ) = 8 ) $@
     @( (Contributed by Kunhao Zheng, 9-Aug-2021.) @)
     mathd-algebra-598 @p |- ( ph -> ( ( ( A x. B ) x. C ) x. D ) = ( 3 / 2 ) ) @=
       ? @.
