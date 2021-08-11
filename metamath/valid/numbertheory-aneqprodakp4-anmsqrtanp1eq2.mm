b18f56f03e714a9b941d646839837a25c5bb16c2
Kunhao ZHENG <39208702+DyeKuu@users.noreply.github.com>
2021-08-11 13:49:03 +0200

    [Metamath] Fix (#42)
    fixes some statements from https://github.com/openai/miniF2F/pull/39

--- metamath/valid/numbertheory-aneqprodakp4-anmsqrtanp1eq2.mm
+++ metamath/valid/numbertheory-aneqprodakp4-anmsqrtanp1eq2.mm
@@ -1,10 +1,11 @@
 $(
   @{
-    numbertheory-aneqprodakp4-anmsqrtanp1eq2.0 @e |- ( ph -> A e. NN ) $@
-    numbertheory-aneqprodakp4-anmsqrtanp1eq2.1 @e |- ( ph -> B e. NN ) $@
-    numbertheory-aneqprodakp4-anmsqrtanp1eq2.2 @e |- ( ph -> E. k e. NN ( 2 ^ k ) = ( ( A + ( B ^ 2 ) ) x. ( B + ( A ^ 2 ) ) ) ) $@
+    numbertheory-aneqprodakp4-anmsqrtanp1eq2.0 @e |- ( ph -> A : NN0 --> RR ) $@
+    numbertheory-aneqprodakp4-anmsqrtanp1eq2.1 @e |- ( ph -> ( A ` 0 ) = 1 ) $@
+    numbertheory-aneqprodakp4-anmsqrtanp1eq2.2 @e |- ( ( ph /\ n e. NN0 ) -> ( A ` ( n + 1 ) ) = ( prod_ k e. ( 0 ... n ) ( A ` k ) + 4 ) ) $@
+    numbertheory-aneqprodakp4-anmsqrtanp1eq2.3 @e |- ( ph -> N e. NN ) $@
     @( (Contributed by Kunhao Zheng, 9-Aug-2021.) @)
-    numbertheory-aneqprodakp4-anmsqrtanp1eq2 @p |- ( ph -> A = 1 ) @=
+    numbertheory-aneqprodakp4-anmsqrtanp1eq2 @p |- ( ph -> ( ( A ` N ) - ( sqrt ` ( A ` ( N + 1 ) ) ) ) = 2 ) @=
       ? @.
   @}
 $)
