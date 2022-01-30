712dc054128d73a33f01bcc54ce1951d295e170e
ccn <ccn@cuppajoeman.com>
2022-01-29 23:25:35 -0500

    add in proof

--- lean/src/test.lean
+++ lean/src/test.lean
@@ -358,7 +358,16 @@ theorem induction_nfactltnexpnm1ngt3
   (h₀ : 3 ≤ n) :
   nat.factorial n < n^(n - 1) :=
 begin
-  sorry
+  induction h₀ with k h₀ IH,
+  { norm_num },
+  {
+    have k_ge_one : 1 ≤ k := le_trans dec_trivial h₀,
+    calc k.succ.factorial = k.succ * k.factorial : rfl
+                      ... < k.succ * k ^ (k-1) : (mul_lt_mul_left (nat.succ_pos k)).mpr IH
+                      ... ≤ k.succ * (k.succ) ^ (k-1): nat.mul_le_mul_left _ $ nat.pow_le_pow_of_le_left (nat.le_succ k) (k-1)
+                      ... = k.succ ^ (k-1 + 1): by rw ← (pow_succ k.succ (k-1))
+                      ... = k.succ ^ k: by rw nat.sub_add_cancel k_ge_one,
+  }
 end
 
 theorem mathd_algebra_208 :
