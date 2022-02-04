8a78f938e0e6ba6efdc03a17f0f30a70f8314bc7
MantasBaksys <baksys.mantas@gmail.com>
2022-02-04 01:04:57 +0000

    fix mathd_algebra_13

--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -122,7 +122,7 @@ end
 
 theorem mathd_algebra_13
   (a b :ℝ)
-  (h₀ : ∀ x, 4 * x / (x^2 - 8 * x + 15) = a / (x - 3) + b / (x - 5)) :
+  (h₀ : ∀ x, (x - 3 ≠ 0 ∧ x - 5 ≠ 0) → 4 * x / (x^2 - 8 * x + 15) = a / (x - 3) + b / (x - 5)) :
   a = -6 ∧ b = 10 :=
 begin
   sorry
