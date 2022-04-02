2b1daad502d914084f079b5f7775cc428b2cdda9
DyeKuu <dyekuu@qq.com>
2022-04-02 12:22:10 +0800

    fix 2013 p8

--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -1131,7 +1131,8 @@ theorem amc12a_2013_p8
   (x y : ℝ)
   (h₀ : x ≠ 0)
   (h₁ : y ≠ 0)
-  (h₂ : x + 2 / x = y + 2 / y) :
+  (h₂ : x ≠ y)
+  (h₃ : x + 2 / x = y + 2 / y) :
   x * y = 2 :=
 begin
   sorry
