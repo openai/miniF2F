0010ebbbd26a4e6dea93fa2d3d3011532e3930e2
DyeKuu <dyekuu@qq.com>
2022-04-02 12:30:36 +0800

    fix mathd-algebra-131

--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -1222,7 +1222,8 @@ theorem mathd_algebra_131
   (f : ℝ → ℝ)
   (h₀ : ∀ x, f x = 2 * x^2 - 7 * x + 2)
   (h₁ : f a = 0)
-  (h₂ : f b = 0) :
+  (h₂ : f b = 0)
+  (h₃ : a ≠ b) :
   1 / (a - 1) + 1 / (b - 1) = -1 :=
 begin
   sorry
