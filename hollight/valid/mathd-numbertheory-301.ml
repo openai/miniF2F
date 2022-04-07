92440f6f706a76e68f85f0960f9c3ddd984cf418
DyeKuu <dyekuu@qq.com>
2022-04-07 14:00:04 +0800

    fix

--- hollight/valid/mathd-numbertheory-301.ml
+++ hollight/valid/mathd-numbertheory-301.ml
@@ -1,5 +1,5 @@
 let mathd-numbertheory-301 = `!j:num.
     ~(j = 0)
 ==>
-    ((3 * (7 * j + 1)) MOD 7 = 2)
+    ((3 * (7 * j + 3)) MOD 7 = 2)
 `;;
--- lean/src/valid.lean
+++ lean/src/valid.lean
@@ -1416,7 +1416,7 @@ end
 
 theorem mathd_numbertheory_301
   (j : ℕ+) :
-  (3 * (7 * ↑j + 1)) % 7 = 2 :=
+  (3 * (7 * ↑j + 3)) % 7 = 2 :=
 begin
   sorry
 end
