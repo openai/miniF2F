06ff3efe4e21ab8c737820dc0d9037277a318348
DyeKuu <dyekuu@qq.com>
2022-04-07 12:17:09 +0800

    fix

--- hollight/test/mathd-numbertheory-227.ml
+++ hollight/test/mathd-numbertheory-227.ml
@@ -1,1 +1,1 @@
-let mathd-numbertheory-227 = `!x:num y:num n:num. (&x / &4 + &y / &6 = &(x + y) / &n) ==> n = 5`;;
+let mathd-numbertheory-227 = `!x:num y:num n:num. (x > 0 /\ y > 0 /\ n > 0) /\ (&x / &4 + &y / &6 = &(x + y) / &n) ==> n = 5`;;
